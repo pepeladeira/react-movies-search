341b27204f2afcab7fb619dd108da5b272870eec
Pedro Ladeira <57876830+pepeladeira@users.noreply.github.com>
2020-05-20 00:28:47 -0300

    Updates

---
+++ precache-manifest.b9ecd9a1285087d9878879c6c9930025.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "036fabaf0592beb43193655765fe5230",
+    "url": "/react-movies-search/index.html"
+  },
+  {
+    "revision": "1030617dc7a0e86b6463",
+    "url": "/react-movies-search/static/css/main.db4eacb5.chunk.css"
+  },
+  {
+    "revision": "f08cdeefd4a643b96253",
+    "url": "/react-movies-search/static/js/2.383c3e0c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-movies-search/static/js/2.383c3e0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1030617dc7a0e86b6463",
+    "url": "/react-movies-search/static/js/main.35936b5f.chunk.js"
+  },
+  {
+    "revision": "f86730b9457bff76e64c",
+    "url": "/react-movies-search/static/js/runtime-main.4966e051.js"
+  }
+]);
